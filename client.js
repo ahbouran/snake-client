fe00a16392c9ce60eb3873d88bb2c25a92ba2443
Ali <alihbouran@gmail.com>
2022-05-11 16:44:31 -0700

    sent a name

--- client.js
+++ client.js
@@ -13,6 +13,11 @@ const connect = function () {
   conn.on('data', (data) => {
     console.log(data.toString())
     })
+  
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server')
+    conn.write("Name: AHB")
+  })
 
 
 
