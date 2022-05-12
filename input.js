2940fb79a7dec7e9b1298702cb5bd142dab86818
Ali <alihbouran@gmail.com>
2022-05-11 18:18:48 -0700

    Added movement keys

--- input.js
+++ input.js
@@ -1,5 +1,7 @@
+let connection;
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -14,8 +16,17 @@ const setupInput = function () {
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit()
+  } else if (key === '\u0077') {
+    connection.write("Move: up")
+  } else if (key === '\u0061') {
+    connection.write("Move: left")
+  } else if (key === '\u0075') {
+    connection.write("Move: down")
+  } else if (key === '\u0064') {
+    connection.write("Move: right")
   }
 };
+
 
 module.exports = {
   setupInput,
--- play.js
+++ play.js
@@ -1,11 +1,12 @@
 const net = require("net");
 const { connect } = require("./client")
 const{ setupInput} = require("./input");
-
+let connection;
+connection = connect()
 console.log("Connecting ...");
-connect();
 
-setupInput()
+
+setupInput(connection)
 
 
 
